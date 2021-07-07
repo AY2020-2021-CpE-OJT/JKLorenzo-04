607ee060ed869309cf125e0700d11dce8d87deee
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-07-07 17:27:59 +0800

    use heroku hosted api

--- phonebook/lib/managers/API.dart
+++ phonebook/lib/managers/API.dart
@@ -2,18 +2,20 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import '../structures/PBData.dart';
 
-final uri = Uri.http('10.0.2.2:3000', '/api/contacts');
-final Map<String, String> header = {
+const _authority = 'jklorenzo-pb-api.herokuapp.com';
+const Map<String, String> _headers = {
   "Content-Type": "application/json",
 };
 
 class API {
   static upsert(PBData data) async {
-    await post(uri, body: jsonEncode(data.toJSON()), headers: header);
+    final _uri = Uri.https(_authority, '/api/contacts');
+    await post(_uri, body: jsonEncode(data.toJSON()), headers: _headers);
   }
 
   static Future<List<PBData>> fetch() async {
-    Response response = await get(uri);
+    final _uri = Uri.https(_authority, '/api/contacts');
+    Response response = await get(_uri, headers: _headers);
     if (response.statusCode != 200) return [];
     List<dynamic> body = jsonDecode(response.body);
     List<PBData> data = body.map((data) => PBData.fromJson(data)).toList();
