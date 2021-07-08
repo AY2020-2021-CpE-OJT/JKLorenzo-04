f21d734b6a31c7d211860482a9c23dfb997d1e56
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-07-08 17:02:18 +0800

    refactor routes to be modular

---
+++ src/routes/api/contacts/get.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { MongoClient } from "mongodb";
+import PBData from "../../../structures/PBData";
+
+export default function (router: Router, client: MongoClient): Router {
+  return router.get("/", async (req, res) => {
+    console.log("GET");
+    try {
+      const data: PBData[] = await client
+        .db("phonebook")
+        .collection("contacts")
+        .find()
+        .sort({ first_name: 1, last_name: 1 })
+        .toArray();
+      await res.json(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      await res.status(error.code ?? 400).send(String(error));
+    }
+  });
+}
