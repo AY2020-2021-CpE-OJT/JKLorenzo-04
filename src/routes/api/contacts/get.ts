84df7cc302424421ea79482af14053a05382b4b4
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-07-09 21:15:00 +0800

    add /api/contacts/get

--- src/routes/api/contacts/get.ts
+++ src/routes/api/contacts/get.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { MongoClient } from "mongodb";
-import PBData from "../../../structures/PBData";
+import express, { MongoClient } from "mongodb";
+import { PBData } from "../../../structures/PBData.js";
 
 export default function (router: Router, client: MongoClient): Router {
   return router.get("/", async (req, res) => {
+    console.log("contacts get all");
     try {
       const data: PBData[] = await client
         .db("phonebook")
@@ -11,10 +12,11 @@ export default function (router: Router, client: MongoClient): Router {
         .find()
         .sort({ first_name: 1, last_name: 1 })
         .toArray();
+
       await res.json(data);
     } catch (error) {
       console.error(error);
-      await res.status(error.code ?? 400).send(String(error));
+      res.status(error.code ?? 400).send(String(error));
     }
   });
 }
