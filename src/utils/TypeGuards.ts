951e4e633ec45c326f7ae4c9c526d378f6df09b0
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-07-10 17:51:55 +0800

    update TypeGuards to throw ExpectError

--- src/utils/TypeGuards.ts
+++ src/utils/TypeGuards.ts
@@ -1,56 +1,51 @@
-import { PBData, PBPartialData } from "../structures/PBData.js";
+import { PBPartialData } from "../structures/PBData.js";
+import { ExpectError } from "./Errors.js";
 
 type PBDataProps = "id" | "first_name" | "last_name" | "phone_numbers";
 
-export function isPBData(data: any): data is PBData {
-  return isPBPartialData(data, [
-    "id",
-    "first_name",
-    "last_name",
-    "phone_numbers",
-  ])
-    ? true
-    : false;
-}
-
-export function isPBPartialData(
+export function expect(
   data: any,
-  expect: PBDataProps[]
-): data is PBPartialData {
+  expected: PBDataProps[],
+  message?: string
+): void {
   const this_data = data as PBPartialData;
 
-  if (expect.includes("id")) {
+  if (expected.includes("id")) {
     if (typeof this_data._id !== "string" || this_data._id.length !== 24) {
-      return false;
+      throw new ExpectError(this_data, message ?? "INVALID_ID");
     }
   }
 
-  if (expect.includes("first_name")) {
+  if (expected.includes("first_name")) {
     if (
       typeof this_data.first_name !== "string" ||
       this_data.first_name.length === 0
     ) {
-      return false;
+      throw new ExpectError(this_data, message ?? "INVALID_FIRST_NAME");
     }
   }
 
-  if (expect.includes("last_name")) {
+  if (expected.includes("last_name")) {
     if (
       typeof this_data.last_name !== "string" ||
       this_data.last_name.length === 0
     ) {
-      return false;
+      throw new ExpectError(this_data, message ?? "INVALID_LAST_NAME");
     }
   }
 
-  if (expect.includes("phone_numbers")) {
+  if (expected.includes("phone_numbers")) {
     if (
       !Array.isArray(this_data.phone_numbers) ||
-      this_data.phone_numbers.some((pnum) => typeof pnum !== "string")
+      this_data.phone_numbers.some(
+        (pnum) => typeof pnum !== "string" || pnum.length === 0
+      )
     ) {
-      return false;
+      throw new ExpectError(this_data, message ?? "INVALID_PHONE_NUMBERS");
     }
   }
+}
 
-  return true;
+export function expectAll(data: any, message?: string): void {
+  expect(data, ["id", "first_name", "last_name", "phone_numbers"], message);
 }
