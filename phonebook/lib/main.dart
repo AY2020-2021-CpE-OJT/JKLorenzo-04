f2231341e23e4c95d9fbc71c01f79e6e248b1022
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-07-11 22:43:49 +0800

    update main

--- phonebook/lib/main.dart
+++ phonebook/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/PBManager.dart';
+import 'screens/Home.dart';
 
 void main() => runApp(PhonebookApp());
 
@@ -8,7 +8,7 @@ class PhonebookApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Phonebook',
-      home: PBManager(),
+      home: Home(),
     );
   }
 }
