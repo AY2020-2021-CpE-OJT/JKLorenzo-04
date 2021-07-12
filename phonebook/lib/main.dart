9e187574409ed45142acacfa42258bf1aa95bef5
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-07-12 17:17:18 +0800

    use dark theme

--- phonebook/lib/main.dart
+++ phonebook/lib/main.dart
@@ -7,6 +7,7 @@ class PhonebookApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       title: 'Phonebook',
       home: Home(),
     );
