7c6cb9703e770d1b763ab4105bca864b8f92dcf8
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-07-13 11:22:12 +0800

    add loading animation to home

--- phonebook/lib/screens/Home.dart
+++ phonebook/lib/screens/Home.dart
@@ -21,6 +21,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _isLoading = true;
   bool _isEditting = false;
   List<PBPartialData> _contacts = [];
   List<String> _selected = [];
@@ -32,8 +33,18 @@ class _HomeState extends State<Home> {
       builder: (context, AsyncSnapshot<List<PBPartialData>> snapshot) {
         if (snapshot.hasError) {
           Toasts.showError(snapshot.error.toString());
+          if (_isLoading == false) {
+            setState(() {
+              _isLoading = true;
+            });
+          }
         } else if (snapshot.hasData && snapshot.data != null) {
           _contacts = snapshot.data!;
+          if (_isLoading == true) {
+            setState(() {
+              _isLoading = false;
+            });
+          }
         }
 
         return Scaffold(
@@ -42,89 +53,95 @@ class _HomeState extends State<Home> {
             backgroundColor: Colors.grey[850],
             title: Text('Contacts'),
             centerTitle: true,
-            actions: _isEditting
-                ? [
-                    TextButton(
-                      child: Text('Cancel'),
-                      onPressed: () {
-                        setState(() {
-                          _isEditting = false;
-                        });
-                      },
-                    )
-                  ]
-                : [
-                    IconButton(
-                      icon: Icon(Icons.add),
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => Create(),
-                          ),
-                        );
-                      },
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.manage_accounts),
-                      onPressed: () {
-                        setState(() {
-                          _selected = [];
-                          _isEditting = true;
-                        });
-                      },
-                    ),
-                  ],
+            actions: _isLoading
+                ? null
+                : _isEditting
+                    ? [
+                        TextButton(
+                          child: Text('Cancel'),
+                          onPressed: () {
+                            setState(() {
+                              _isEditting = false;
+                            });
+                          },
+                        )
+                      ]
+                    : [
+                        IconButton(
+                          icon: Icon(Icons.add),
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => Create(),
+                              ),
+                            );
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.manage_accounts),
+                          onPressed: () {
+                            setState(() {
+                              _selected = [];
+                              _isEditting = true;
+                            });
+                          },
+                        ),
+                      ],
           ),
           body: Center(
-            child: ListView.builder(
-              itemCount: _contacts.length,
-              itemBuilder: (context, index) {
-                final this_data = _contacts[index];
-                return ListTile(
-                  horizontalTitleGap: 5,
-                  leading: Icon(
-                    Icons.person,
-                    color: Colors.white,
-                    size: 50,
-                  ),
-                  title: Text(
-                    '${this_data.first_name} ${this_data.last_name}',
-                    style: TextStyle(
-                        color: Colors.grey[200],
-                        fontSize: 18,
-                        letterSpacing: 1),
-                  ),
-                  trailing: _isEditting
-                      ? Icon(
-                          _selected.contains(this_data.id)
-                              ? Icons.radio_button_checked
-                              : Icons.radio_button_unchecked,
-                          color: Colors.lightBlue)
-                      : null,
-                  onTap: () {
-                    if (_isEditting) {
-                      setState(() {
-                        if (_selected.contains(this_data.id)) {
-                          _selected.remove(this_data.id);
-                        } else {
-                          _selected.add(this_data.id!);
-                        }
-                      });
-                    } else {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => View(
-                            id: this_data.id!,
-                          ),
+            child: _isLoading
+                ? CircularProgressIndicator()
+                : ListView.builder(
+                    itemCount: _contacts.length,
+                    itemBuilder: (context, index) {
+                      final this_data = _contacts[index];
+                      return ListTile(
+                        horizontalTitleGap: 5,
+                        leading: Icon(
+                          Icons.person,
+                          color: Colors.white,
+                          size: 50,
                         ),
+                        title: Text(
+                          '${this_data.first_name} ${this_data.last_name}',
+                          style: TextStyle(
+                              color: Colors.grey[200],
+                              fontSize: 18,
+                              letterSpacing: 1),
+                        ),
+                        trailing: _isEditting
+                            ? Icon(
+                                _selected.contains(this_data.id)
+                                    ? Icons.radio_button_checked
+                                    : Icons.radio_button_unchecked,
+                                color: Colors.lightBlue)
+                            : null,
+                        onTap: () {
+                          if (_isEditting) {
+                            setState(() {
+                              if (_selected.contains(this_data.id)) {
+                                _selected.remove(this_data.id);
+                              } else {
+                                _selected.add(this_data.id!);
+                              }
+                            });
+                          } else {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => View(
+                                  id: this_data.id!,
+                                ),
+                              ),
+                            );
+                          }
+                        },
                       );
-                    }
-                  },
-                );
-              },
-            ),
+                    },
+                  ),
           ),
-          floatingActionButton: _isEditting && _selected.length > 0
+          floatingActionButton: !_isLoading &&
+                  _isEditting &&
+                  _selected.length > 0
               ? FloatingActionButton.extended(
                   backgroundColor: Colors.red,
                   icon: Icon(
